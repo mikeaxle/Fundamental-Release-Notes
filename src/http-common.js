a17539e0242be4741c1efb0e2af33b20edee6f10
mikeaxle <mikeaxle2006@gmail.com>
2018-05-03 14:50:51 +0200

    url temp

--- src/http-common.js
+++ src/http-common.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
 
 export const HTTP = axios.create({
-  baseURL: 'https://cors-anywhere.herokuapp.com/http://fundamental.net/releasenotes/api.php'
+  baseURL: 'https://cors-anywhere.herokuapp.com/http://fundamental.net/releasenotes/api.php',
+  headers: {'Access-Control-Allow-Origin': '*'}
 })
